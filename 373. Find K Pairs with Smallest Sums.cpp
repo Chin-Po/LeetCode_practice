8e5b8fb0bbeeba21c2ac47ecdd53025505c4dbcb
CP-Tsai <aupt23@gmail.com>
2018-01-01 12:27:34 -0500

    Add files via upload

---
+++ 373. Find K Pairs with Smallest Sums.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        vector<pair<int, int>> res;
+        for (int i = 0; i < min((int)nums1.size(), k); ++i) {
+            for (int j = 0; j < min((int)nums2.size(), k); ++j) {
+                res.push_back({nums1[i], nums2[j]});
+            }
+        }
+        sort(res.begin(), res.end(), [](pair<int, int> &a, pair<int, int> &b)->bool{return a.first + a.second < b.first + b.second;});
+        if (res.size() > k) res.erase(res.begin() + k, res.end());
+        return res;
+    }
+};
