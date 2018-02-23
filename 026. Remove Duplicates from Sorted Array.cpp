3bd734a7baa222197545373e1d5c95edab857fe5
CP-Tsai <aupt23@gmail.com>
2018-02-23 09:48:52 -0500

    Delete 026. Remove Duplicates from Sorted Array.cpp

--- 026. Remove Duplicates from Sorted Array.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-        auto iter = unique(nums.begin(), nums.end());
-        nums.erase(iter, nums.end());
-        return nums.size();
-    }
-};
