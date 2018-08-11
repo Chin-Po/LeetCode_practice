c3cca3c5ea9c0f127d863b7b9d44656ceea050f6
Chin-Po <ChinPo.Tsai@magna.com>
2018-08-11 10:19:12 -0400

    delete files after 850

--- 852. Peak Index in a Mountain Array.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    int peakIndexInMountainArray(vector<int>& A) {
-        for(int i = 1; i < A.size()-1; i++){
-            if(A[i-1] < A[i] && A[i] > A[i+1]) return i;
-        }
-    }
-};
